cc16041147dfb4572470a57dd4af59adadf7928e
gabe <gabe1118@yahoo.com>
2018-11-27 21:50:42 -0500

    to django?

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: gunicorn app:app
-
+web: gunicorn fraud-server:wsgi
---
+++ fraud_game/apps.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.apps import AppConfig
+
+
+class FraudGameConfig(AppConfig):
+    name = 'fraud_game'
