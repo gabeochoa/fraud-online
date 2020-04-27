7a8c7541777917b5b16083310ef0cd63ed742d84
gabeochoa <gabeochoa@fb.com>
2020-04-26 21:00:39 -0400

    about functional

--- react_frontend/src/fakeartist/FakeArtistAbout.jsx
+++ react_frontend/src/fakeartist/FakeArtistAbout.jsx
@@ -2,13 +2,8 @@ import React, { Component } from "react";
 import autobind from "autobind-decorator";
 import About from "../components/About";
 
-@autobind
-class FakeArtistAbout extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  handleClick(event) {
+const FakeArtistAbout = (props) => {
+  const handleClick = (event) => {
     while (event.target.getAttribute("name") === null) {
       event.target = event.target.parentNode;
     }
@@ -16,7 +11,7 @@ class FakeArtistAbout extends Component {
     // console.log("button was clicked : " + button);
     switch (button) {
       case "about_back":
-        this.props.changeLocation("_back", () => {});
+        props.changeLocation("_back", () => { });
         break;
       default:
         console.log("button was clicked : " + button);
@@ -24,86 +19,53 @@ class FakeArtistAbout extends Component {
     }
   }
 
-  render_header() {
-    return <p></p>;
-  }
-
-  render_body() {
-    return (
-      <div
-        style={{
-          textAlign: "left",
-        }}
+  return (
+    <>
+      <About
+        header_text={<p></p>}
+        body={
+          <div style={{ textAlign: "left", }} >
+            <ul style={{ listStyleType: "cjk-ideographic", }} >
+              <li>Fake Artist is based on the japanese board game:</li>
+              <a href="https://oinkgms.com/en/a-fake-artist-goes-to-new-york">
+                A Fake Artist Goes To New York
+              </a>
+              <li>
+                Whoever draws first will keep in mind some object or person or thing
+                that reminds them of the location.
+              </li>
+              <li>Players take turns adding one line per round to the drawing.</li>
+              <li>
+                However there is one spy amongst the players who is trying to figure
+                out the location.
+              </li>
+              <li>
+                After all the rounds are over, all players vote on who they think is
+                the spy.
+              </li>
+              <li>If the real spy gets the most votes they lose. </li>
+              <li> However if the spy guesses correctly what the place was, then they win. </li>
+            </ul>
+          </div>
+        }
+        footer_text={
+          <h6 style={{ fontSize: "0.75em", }} >
+            If you enjoyed this game, please check out the physical version linked
+            above :)
+          </h6>
+        }
+      />
+      <hr />
+      <a
+        name="about_back"
+        className="button is-outlined button_style button_font"
+        onClick={handleClick}
+        style={button_stretch}
       >
-        <ul
-          style={{
-            listStyleType: "cjk-ideographic",
-          }}
-        >
-          <li>Fake Artist is based on the japanese board game:</li>
-
-          <a href="https://oinkgms.com/en/a-fake-artist-goes-to-new-york">
-            A Fake Artist Goes To New York
-          </a>
-
-          <li>
-            Whoever draws first will keep in mind some object or person or thing
-            that reminds them of the location.
-          </li>
-
-          <li>Players take turns adding one line per round to the drawing.</li>
-          <li>
-            However there is one spy amongst the players who is trying to figure
-            out the location.
-          </li>
-
-          <li>
-            After all the rounds are over, all players vote on who they think is
-            the spy.
-          </li>
-          <li>If the real spy gets the most votes they lose. </li>
-          <li>
-            However if the spy guesses correctly what the place was, then they
-            win.{" "}
-          </li>
-        </ul>
-      </div>
-    );
-  }
-
-  render_footer() {
-    return (
-      <h6
-        style={{
-          fontSize: "0.75em",
-        }}
-      >
-        If you enjoyed this game, please check out the physical version linked
-        above :){" "}
-      </h6>
-    );
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-        <About
-          header_text={this.render_header()}
-          body={this.render_body()}
-          footer_text={this.render_footer()}
-        />
-        <hr />
-        <a
-          name="about_back"
-          className="button is-outlined button_style button_font"
-          onClick={this.handleClick}
-          style={button_stretch}
-        >
-          Back
+        Back
         </a>
-      </React.Fragment>
-    );
-  }
+    </>
+  );
 }
 
 const button_stretch = {
